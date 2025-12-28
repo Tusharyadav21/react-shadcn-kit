a2b1d62337b078ad37b0bd147404f2563a256278
tusharyadav21 <tusharydv@hotmail.com>
2025-12-28 05:53:59 +0530

    feat: bump version and export `useTheme` and `useBrowserCacheImage` hooks.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,13 @@
+export * from "./use-theme";
 export * from "./use-mobile";
 export * from "./use-aria-live";
 export * from "./use-focus-visible";
 export * from "./use-screen-reader-only";
+export * from "./use-browser-cache-image";
 
+export type * from "./use-theme";
 export type * from "./use-mobile";
 export type * from "./use-aria-live";
 export type * from "./use-focus-visible";
 export type * from "./use-screen-reader-only";
+export type * from "./use-browser-cache-image";
